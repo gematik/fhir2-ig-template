8a0a1bf95878d64ace9a68edbcffedb846454e07
Ronald Martins <ronald.martins@gematik.de>
2026-07-01 10:38:31 +0200

    first commit

---
+++ ReleaseNotes.md
@@ -0,0 +1,9 @@
+<img align="right" width="250" height="47" alt="gematik GmbH" src="img/Gematik_Logo_Flag_With_Background.png"/> <br/>
+
+# Release Notes fhir2-ig-template
+
+### Version 0.0.1
+- Initial release
+
+
+<br>
---
+++ content/assets/js/ig.bibliography.gematik.js
@@ -0,0 +1,16 @@
+---
+---
+const literatureData = [
+    {% for entry in site.data.bibliography.entries %}
+    {
+        key: "{{ entry.key }}",
+        link: "{{ entry.link }}",
+        title: "{{ entry.title }}",
+        author: "{{ entry.author }}"
+    }{% if forloop.last %}{% else %},{% endif %}
+    {% endfor %}
+];
+
+document.addEventListener("DOMContentLoaded", function () {
+    igtools.convertBibliographyToLink(literatureData);
+});
